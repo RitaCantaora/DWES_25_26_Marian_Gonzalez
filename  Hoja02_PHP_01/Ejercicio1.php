a356287cdcb0ead16eda959cbf7ecbf339a596f3
RitaCantaora <mdegs02@educastur.es>
2025-09-29 18:24:14 +0200

    commit

---  Hoja02_PHP_01/Ejercicio1.php
+++  Hoja02_PHP_01/Ejercicio1.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+   
 <?php
 
+$instituto="Instituto Monte Naranco";
+$ciudad="Oviedo";
+
+echo "<strong>$instituto</strong> está en la ciudad de $ciudad.";
 
 
-
-?>
+?> 
+</body>
+</html>
