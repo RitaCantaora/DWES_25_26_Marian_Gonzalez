2a758099085cccdc0930346b14c7243b6b13bdc6
RitaCantaora <mdegs02@educastur.es>
2025-09-29 19:44:32 +0200

    Initial commit

---  Hoja02_PHP_02/Ejercicio1.php
+++  Hoja02_PHP_02/Ejercicio1.php
@@ -3,12 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio1</title>
+    <title>Ejercicio 1</title>
 </head>
 <body>
     <?php
-    
+    echo "Conversor de grados Celsius a Fahrenheit<br>";
 
-    ?>
+    //variable de grados 
+    $grados=25;
+
+    //formula para el resultado
+    $resultado = ($grados * 9/5) + 32;
+
+echo "$grados celsius equivalen a $resultado fahrenheit. <br><br>" ;
+
+  //  variable usando gettype()
+   
+    echo "Variable de grados : " . gettype($grados) . "<br>";
+    echo "Variable de resultado : " . gettype($resultado) . "<br><br>";
+
+    //  var_dump()
+   
+    echo "Variable de grados  : ";
+    var_dump($grados);
+    echo "<br>Variable de resultado : ";
+    var_dump($resultado);
+
+?>
+
 </body>
 </html>
---
+++  Hoja02_PHP_02/Ejercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio2</title>
+</head>
+<body>
+    <?php
+
+$numero1=5;
+$numero2=3;
+$numero3=4;
+$media=($numero1+$numero2+$numero3)/3;
+echo "La media de $numero1 + $numero2 + $numero3 = $media"
+
+
+?>
+</body>
+</html>
