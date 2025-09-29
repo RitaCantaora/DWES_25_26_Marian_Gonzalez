8583db151ba650dda074ad823d692ef5b5fae70f
RitaCantaora <mdegs02@educastur.es>
2025-09-29 18:58:29 +0200

    prueba

---
+++  Hoja02_PHP_01/Ejercicio2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercico 2</title>
+</head>
+<body>
+    <?php
+$numero1=-2;
+$numero2=5;
+$resultado=$numero1 * $numero2;
+
+echo "El resultado de multiplicar $numero1  y $numero2 =$resultado"
+
+?>
+</body>
+</html>
